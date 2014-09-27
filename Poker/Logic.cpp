5930df3e7d9af2845d797bd34951942ed367fb24
Alek86 <bukaevav@gmail.com>
2014-09-27 14:18:39 -0400

    Added High Card

--- Poker/Logic.cpp
+++ Poker/Logic.cpp
@@ -654,6 +654,19 @@ CompareResult::Value CompareOnePair(const Hand& first, const Hand& second)
 
 CompareResult::Value CompareHighCard(const Hand& first, const Hand& second)
 {
+    for (size_t i = 0; i < first.size(); ++i)
+    {
+        if (GetRankInt(first[i]) > GetRankInt(second[i]))
+        {
+            return CompareResult::FirstWon;
+        }
+
+        if (GetRankInt(first[i]) < GetRankInt(second[i]))
+        {
+            return CompareResult::SecondWon;
+        }
+    }
+
     return CompareResult::BothWon;
 }
 
